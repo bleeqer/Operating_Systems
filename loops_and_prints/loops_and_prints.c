a0060ef78e38e4bb17d1e1d0090c67fcf1fec7c6
bleeqer <bleeqtheproducer@gmail.com>
2022-07-10 10:42:16 +0900

    loops_and_prints

---
+++ loops_and_prints/loops_and_prints.c
@@ -0,0 +1,28 @@
+#include <stdio.h> // I/O
+#include <stdlib.h> // C standard library
+#include <sys/time.h> // time related
+#include <assert.h> // assertion macro 
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2) 
+    {
+        // write into the file pointer(stderr)
+        // stderr is the defualt file descriptor where a process can write error messages
+        fprintf(stderr, "usage: cpu <string>\n");
+    }
+
+    char *str = argv[1];
+
+    while (1)
+    {
+        printf("%s\n", str);
+    }
+
+    return 0;
+}
+
+/*
+    segmentation fault: caused by a program trying to read or write an illegal memory location
+    core dump: a file of a computer's documented memory of when a program or computer crashed
+*/
